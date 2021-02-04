6b38545c5b2071e7e173fcc326082706d393fca3
Sven Lechner <SirWindfield@users.noreply.github.com>
2021-02-05 00:29:18 +0100

    refactor: remove duplicate code using macro

--- src/lib.rs
+++ src/lib.rs
@@ -3,114 +3,41 @@
 #[macro_use]
 extern crate napi_derive;
 
-use napi::{ContextlessResult, Env, JsObject, JsString, Result};
+macro_rules! create_named_method_for_folder_names {
+    ($($folder_name:ident),+) => {
+        use paste::paste;
 
-mod r#impl {
-    use directories_next::UserDirs;
-    use std::path::PathBuf;
+        paste! {
+            #[module_exports]
+            fn init(mut exports: napi::JsObject) -> napi::Result<()> {
+                $(
+                    exports.create_named_method(stringify!([<$folder_name Folder>]), [<$folder_name _folder>])?;
+                )+
 
-    pub fn get_user_audio_folder() -> Option<PathBuf> {
-        UserDirs::new().and_then(|dirs| dirs.audio_dir().map(Into::into))
-    }
+                Ok(())
+            }
 
-    pub fn get_user_desktop_folder() -> Option<PathBuf> {
-        UserDirs::new().and_then(|dirs| dirs.desktop_dir().map(Into::into))
-    }
+            mod r#impl {
+                $(
+                    pub fn [<get_user_ $folder_name _folder>]() -> Option<std::path::PathBuf> {
+                        directories_next::UserDirs::new().and_then(|dirs| dirs.[<$folder_name _dir>]().map(Into::into))
+                    }
+                )+
+            }
 
-    pub fn get_user_document_folder() -> Option<PathBuf> {
-        UserDirs::new().and_then(|dirs| dirs.document_dir().map(Into::into))
-    }
+            $(
+                #[contextless_function]
+                fn [<$folder_name _folder>](ctx: napi::Env) -> napi::ContextlessResult<napi::JsString> {
+                    let res = r#impl::[<get_user_ $folder_name _folder>]()
+                        .map(|p| p.display().to_string())
+                        .map(|s| ctx.create_string(&s))
+                        .and_then(Result::ok);
 
-    pub fn get_user_download_folder() -> Option<PathBuf> {
-        UserDirs::new().and_then(|dirs| dirs.download_dir().map(Into::into))
-    }
-
-    pub fn get_user_picture_folder() -> Option<PathBuf> {
-        UserDirs::new().and_then(|dirs| dirs.picture_dir().map(Into::into))
-    }
+                    Ok(res)
+                }
+            )+
+        }
+    };
 }
 
-#[module_exports]
-fn init(mut exports: JsObject) -> Result<()> {
-    exports.create_named_method("audioFolder", audio_folder)?;
-    exports.create_named_method("documentFolder", document_folder)?;
-    exports.create_named_method("desktopFolder", desktop_folder)?;
-    exports.create_named_method("downloadFolder", download_folder)?;
-    exports.create_named_method("pictureFolder", picture_folder)?;
-    Ok(())
-}
-
-#[contextless_function]
-fn audio_folder(ctx: Env) -> ContextlessResult<JsString> {
-    let res = r#impl::get_user_audio_folder()
-        .map(|p| p.display().to_string())
-        .map(|s| ctx.create_string(&s));
-
-    Ok(match res {
-        Some(inner) => match inner {
-            Ok(s) => Some(s),
-            Err(_) => None,
-        },
-        None => None,
-    })
-}
-
-#[contextless_function]
-fn document_folder(ctx: Env) -> ContextlessResult<JsString> {
-    let res = r#impl::get_user_document_folder()
-        .map(|p| p.display().to_string())
-        .map(|s| ctx.create_string(&s));
-
-    Ok(match res {
-        Some(inner) => match inner {
-            Ok(s) => Some(s),
-            Err(_) => None,
-        },
-        None => None,
-    })
-}
-
-#[contextless_function]
-fn desktop_folder(ctx: Env) -> ContextlessResult<JsString> {
-    let res = r#impl::get_user_desktop_folder()
-        .map(|p| p.display().to_string())
-        .map(|s| ctx.create_string(&s));
-
-    Ok(match res {
-        Some(inner) => match inner {
-            Ok(s) => Some(s),
-            Err(_) => None,
-        },
-        None => None,
-    })
-}
-
-#[contextless_function]
-fn download_folder(ctx: Env) -> ContextlessResult<JsString> {
-    let res = r#impl::get_user_download_folder()
-        .map(|p| p.display().to_string())
-        .map(|s| ctx.create_string(&s));
-
-    Ok(match res {
-        Some(inner) => match inner {
-            Ok(s) => Some(s),
-            Err(_) => None,
-        },
-        None => None,
-    })
-}
-
-#[contextless_function]
-fn picture_folder(ctx: Env) -> ContextlessResult<JsString> {
-    let res = r#impl::get_user_picture_folder()
-        .map(|p| p.display().to_string())
-        .map(|s| ctx.create_string(&s));
-
-    Ok(match res {
-        Some(inner) => match inner {
-            Ok(s) => Some(s),
-            Err(_) => None,
-        },
-        None => None,
-    })
-}
+create_named_method_for_folder_names!(audio, desktop, document, download, picture);
